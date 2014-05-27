f6d4a7d339e1169dcfc024b7dcb2558d00d0f7ee
Kilo9 <jbassi@stevens.edu>
2014-05-27 16:17:54 -0400

    nested grid tests

--- layout-cssgrid-polyfill/test/grid.test.js
+++ layout-cssgrid-polyfill/test/grid.test.js
@@ -25,7 +25,7 @@ define([
         });
 
         it('correctly places and sizes the only element with fr', function () {
-            helper.expectGridElement('single_fr', {
+            helper.expectGridElement('single__cell--fr', {
                 left: '0px',
                 top: '0px',
                 width: '400px',
@@ -34,12 +34,105 @@ define([
         });
 
         it('correctly places and sizes the only element with auto', function () {
-            helper.expectGridElement('single_auto', {
+            helper.expectGridElement('single__cell--auto', {
                 left: '0px',
                 top: '0px',
-                width: '400px',
-                height: '400px'
-            })
+                width: '0px',
+                height: '0px'
+            });
         })
+
+        it('correctly places and sizes the only element with px', function () {
+            helper.expectGridElement('single__cell--px', {
+                left: '0px',
+                top: '0px',
+                width: '100px',
+                height: '100px',
+            });
+        });
+    });
+
+    describe('A nested grid loaded from a template with a separate inner div for the inner grid', function () {
+
+        //initial setup
+        beforeEach(function () {
+            core.layout.parseGridStyles(function () {
+                core.layout.invalidate();
+            });
+
+            waits(200) //give time to snap into place
+        });
+
+        it('correctly sets up outer grid', function () {
+            helper.expectGridElement('outer__top', {
+                left: '0px',
+                top: '0px',
+                width: '800px',
+                height: '300px'
+            });
+            helper.expectGridElement('outer__bottom', {
+                left: '0px',
+                top: '300px',
+                width: '800px',
+                height: '500px'
+            });
+        });
+
+        it('correctly sets up inner grid', function () {
+            helper.expectInnerGridElement('inner__top', 3, {
+                left: '0px',
+                top: '300px',
+                width: '800px',
+                height: '250px'
+            });
+            helper.expectInnerGridElement('inner__bottom', 3, {
+                left: '0px',
+                top: '550px',
+                width: '800px',
+                height: '250px'
+            });
+        });
+    });
+
+    describe('A nested grid loaded from a template with the bottom cell as the inner grid', function () {
+
+        //initial setup
+        beforeEach(function () {
+            core.layout.parseGridStyles(function () {
+                core.layout.invalidate();
+            });
+
+            waits(200) //give time to snap into place
+        });
+
+        it('correctly sets up outer grid', function () {
+            helper.expectGridElement('outer__top--c', {
+                left: '0px',
+                top: '0px',
+                width: '800px',
+                height: '300px'
+            });
+            helper.expectGridElement('outer__bottom-grid--c', {
+                left: '0px',
+                top: '300px',
+                width: '800px',
+                height: '500px'
+            });
+        });
+
+        it('correctly sets up inner grid', function () {
+            helper.expectInnerGridElement('inner__top--c', 2, {
+                left: '0px',
+                top: '300px',
+                width: '800px',
+                height: '250px'
+            });
+            helper.expectInnerGridElement('inner__bottom--c', 2, {
+                left: '0px',
+                top: '550px',
+                width: '800px',
+                height: '250px'
+            });
+        });
     });
 });
--- layout-cssgrid-polyfill/test/gridTestHelper.js
+++ layout-cssgrid-polyfill/test/gridTestHelper.js
@@ -32,7 +32,53 @@ define([
         }
     }
 
+
+    
+    /*
+    * Goes finds the level-th parent of the element to calculate the coordinates/dimensions
+    */
+    function expectInnerGridElement(element_id, level, expected) {
+        var element,
+            element_rect,
+            ancestor,
+            ancestor_rect,
+            style,
+            i;
+
+        element = document.getElementById(element_id);
+        ancestor = element;
+        console.log(element);
+
+        element_rect = element.getBoundingClientRect();
+
+        for (i = 0; i < level; i++) {
+            ancestor = ancestor.parentElement;
+        }
+
+        ancestor_rect = ancestor.getBoundingClientRect();
+
+        console.log(element_id + "  " + element_rect.left + " " + element_rect.top + " " + element_rect.height + " " + element_rect.width);
+        console.log("gr: " + ancestor_rect.left + " " + ancestor_rect.top + " " + ancestor_rect.height + " " + ancestor_rect.width);
+
+        if (expected['left'] !== undefined) {
+            expect((element_rect.left - ancestor_rect.left) + 'px').toBe(expected['left']);
+        }
+
+        if (expected['top'] !== undefined) {
+            expect((element_rect.top - ancestor_rect.top) + 'px').toBe(expected['top']);
+        }
+
+        if (expected['width'] !== undefined) {
+            expect(element_rect.width.toString() + 'px').toBe(expected['width']);
+        }
+
+        if (expected['height'] !== undefined) {
+            expect(element_rect.height.toString() + 'px').toBe(expected['height']);
+        }
+    }
+
     return {
-        expectGridElement: expectGridElement
+        expectGridElement: expectGridElement,
+        expectInnerGridElement: expectInnerGridElement
     };
 });
